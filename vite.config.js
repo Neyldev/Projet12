b2d46009a62dc9b5526786e79394651d66a136ce
Neyl <neyltv@gmail.com>
2025-03-06 17:15:21 +0100

    Test de mise en ligne Github

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    base: '/',
+    base: '/Projet12/',
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
